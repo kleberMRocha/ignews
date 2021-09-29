9ef955b8f3fdf1fcd6fc61df1224382284dbeba2
kleber <kleberrocha2701@gmail.com>
2021-09-28 23:56:40 -0300

    Pagamentos no Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,10 +1,36 @@
+import {useSession, signIn} from 'next-auth/client';
+import {api} from '../../services/axios';
 import React from 'react';
 import styles from './style.module.scss';
+import { getStripeJs } from '../../services/stripe.js';
 
 interface ISubProps {
   priceId: string;
 }
 
 export const SubButton: React.FC<ISubProps> = ({ priceId }) => {
-  return <button className={styles.subscribeButton}>Subscribe now</button>;
+const [session] =  useSession();
+
+const handleSubscribe = async () => {
+  if(!session){
+    signIn('github');
+    return;
+  } 
+ 
+  try {
+   const response = await api.post(`subscribe/${priceId}`);
+   const { customer_id } =  response.data;
+
+   const stripe = await getStripeJs();
+
+   await stripe.redirectToCheckout({sessionId: customer_id});
+
+  } catch (error) {
+    console.log(error);
+    
+  }
+  // todo criar checkout 
+};
+
+  return <button  onClick={() => handleSubscribe()} className={styles.subscribeButton}>Subscribe now</button>;
 };
---
+++ src/pages/api/subscribe/[id].ts
@@ -0,0 +1,67 @@
+import {NextApiRequest,NextApiResponse, } from 'next';
+import { stripe } from '../../../services/stripe';
+import {getSession} from 'next-auth/client';
+import { fauna } from '../../../services/fauna';
+import {query as q} from 'faunadb';
+
+type IUser = {
+  ref:{
+    id: string;
+  },
+  data:{
+    stripe_customer_id: string;
+  }
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const {id} = req.query;
+  const session = await getSession({req});
+
+  const stripeCostumer = await stripe.customers.create({
+    email: session.user.email,
+  });
+
+  const user = await fauna.query<IUser>(
+    q.Get(q.Match(
+      q.Index('by_email'),
+      q.Casefold(session.user.email),
+    ))
+  );
+
+  let customerStripeId = user.data.stripe_customer_id;
+  
+  if(!customerStripeId){
+    await fauna.query(
+      q.Update(
+        q.Ref(q.Collection('users'),user.ref.id),
+        {
+          data: {
+            stripe_customer_id: stripeCostumer.id,
+          }
+        }
+      )
+    );
+
+    customerStripeId = stripeCostumer.id;
+  }
+
+  if(req.method === 'POST'){
+    const checkoutSession = await stripe.checkout.sessions.create({
+      customer: customerStripeId,
+      payment_method_types:['card'],
+      billing_address_collection: 'required',
+      line_items: [{price: String(id), quantity: 1}],
+      mode:'subscription',
+      allow_promotion_codes: true,
+      success_url: 'http://localhost:3000/posts',
+      cancel_url: 'http://localhost:3000/'
+    });
+    return res.status(200).json({
+      customer_id: checkoutSession.id
+    });
+  }
+
+  res.setHeader('Allow', 'POST');
+  res.status(405).end('Method not allowed');
+ 
+}
